fb8b1675ac763d9b2e0343c80c3e17870b25f291
David Briscoe <idbrii@gmail.com>
2018-02-23 17:14:31 -0800

    Remove unnecessary argument passing
    From :help snip
        'snip.buffer' - alias for 'vim.current.window.buffer'

--- pythonx/snip_cs.py
+++ pythonx/snip_cs.py
@@ -1,20 +1,21 @@
 import re
+import vim
 
-def _search_and_return_first_group_from_regex(regex, snip):
-    for line in snip.buffer:
+def _search_and_return_first_group_from_regex(regex):
+    for line in vim.current.window.buffer:
         match = regex.match(line)
         if match:
             return match.group(1)
     return None
 
-def get_cs_classname(snip):
-    return _search_and_return_first_group_from_regex(re.compile(".*class (\w*)"), snip)
+def get_cs_classname():
+    return _search_and_return_first_group_from_regex(re.compile(".*class (\w*)"))
 
-def get_cs_parentclass(snip):
-    return _search_and_return_first_group_from_regex(re.compile(".*class \w+ : (\w+)"), snip)
+def get_cs_parentclass():
+    return _search_and_return_first_group_from_regex(re.compile(".*class \w+ : (\w+)"))
 
-def is_parent_class_unityobject(snip):
-    parent = get_cs_parentclass(snip)
+def is_parent_class_unityobject():
+    parent = get_cs_parentclass()
     if parent:
         valid_this = ['MonoBehaviour', 'ScriptableObject']
         return any([p_class in parent for p_class in valid_this])
