f2bad90d5a4fa52e4d222d365180a48cc7c1a4a6
David Briscoe <idbrii@gmail.com>
2021-09-17 11:48:09 -0700

    lua: Look at above functions to guess signature
    Instead of looking from the top of the file, first try looking from the
    current line up to see if an existing function or class definition
    exist.
    
    We want to define a function for the same class as the above function or
    the closest class.
    
    Add some tests since that makes snippet dev so much easier. I use pytest
    now, but these tests could be run manually too.

--- pythonx/snip_lua.py
+++ pythonx/snip_lua.py
@@ -3,24 +3,38 @@
 import re
 
 
-def _search_and_return_first_group_from_regex(buffer, regex):
+def _search_and_return_groups_from_regex(buffer, regex):
     for line in buffer:
         match = regex.match(line)
         if match:
-            return match.group(1)
-    return None
+            g = list(match.groups())
+            g.append(None)
+            g.append(None)
+            return g[0], g[1]
+    return None, None
 
 
-def get_lua_classname(basename, buffer):
-    # class.lua
-    tablename = _search_and_return_first_group_from_regex(
-        buffer, re.compile("local (\w*) = [cC]lass")
+def get_lua_classname(basename, buffer, cursor):
+    cur_line = cursor[0]
+    lines = buffer[:cur_line]
+    lines.reverse()
+    tablename, invoker = _search_and_return_groups_from_regex(
+        lines, re.compile(r"^function (\w+)([:\.])(\w+)")
     )
-    invoker = ":"
+
+    class_re = re.compile(r"local (\w*) = [cC]lass")
+    if not tablename:
+        # class.lua
+        tablename, _ = _search_and_return_groups_from_regex(lines, class_re)
+        invoker = ":"
+    if not tablename:
+        # class.lua
+        tablename, _ = _search_and_return_groups_from_regex(buffer, class_re)
+        invoker = ":"
     if not tablename:
         # Table-scoped functions.
-        tablename = _search_and_return_first_group_from_regex(
-            buffer, re.compile("local (" + basename + ") = ", re.IGNORECASE)
+        tablename, _ = _search_and_return_groups_from_regex(
+            buffer, re.compile(r"local (" + basename + ") = ", re.IGNORECASE)
         )
         invoker = "."
     if not tablename:
@@ -28,3 +42,78 @@ def get_lua_classname(basename, buffer):
         tablename = ""
         invoker = ""
     return tablename, invoker
+
+
+# pytest
+#
+def _testwrap(bufstr):
+    buf = bufstr.split("\n")
+    return buf, [0, len(buf)]
+
+
+def test_get_lua_classname():
+    buf, endcursor = _testwrap(
+        """
+local PopupDialog = require("screens/dialogs/popupdialog")
+
+local EquipDialog = Class(PopupDialog, function(self, controller)
+    self._base._ctor(self, "EquipDialog", controller)
+    self:Init()
+end)
+
+local EquipDialogIcon = Class(Image, function()
+    self._base._ctor(self, "EquipDialogIcon")
+    self.root = Image()
+end)
+
+function EquipDialog:Init()
+    self.root = Widget()
+end
+
+"""
+    )
+    r = get_lua_classname("equipdialog", buf, endcursor)
+    assert r[0] == "EquipDialog"
+    assert r[1] == ":"
+
+    after_equip_class = 7
+    after_icon_class = 12
+    after_equip_func = 16
+    r = get_lua_classname("equipdialog", buf, [0, after_equip_class])
+    assert buf[after_equip_class] == ""
+    assert r[0] == "EquipDialog"
+    assert r[1] == ":"
+    r = get_lua_classname("equipdialog", buf, [0, after_icon_class])
+    assert buf[after_icon_class] == ""
+    assert r[0] == "EquipDialogIcon"
+    assert r[1] == ":"
+    r = get_lua_classname("equipdialog", buf, [0, after_equip_func])
+    assert buf[after_equip_func] == ""
+    assert r[0] == "EquipDialog"
+    assert r[1] == ":"
+
+    buf, endcursor = _testwrap(
+        """
+local not_template = {}
+
+function hi()
+    return "hello"
+end
+"""
+    )
+    r = get_lua_classname("template", buf, endcursor)
+    assert r[0] == ""
+    assert r[1] == ""
+
+    buf, endcursor = _testwrap(
+        """
+local template = {}
+
+local function hi()
+    return "hello"
+end
+"""
+    )
+    r = get_lua_classname("template", buf, endcursor)
+    assert r[0] == "template"
+    assert r[1] == "."
